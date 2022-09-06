4370a2ae8e5ff954a6d6f6d9925ed7a32644c628
lynn840429 <lynn840429@gmail.com>
2022-09-06 15:30:11 +0800

    [2022/09/06 lynn840429] Ver: 1.11.0; Info: Day05; Todo: None

--- Class/Day01-15/code/Day05/prime.py
+++ Class/Day01-15/code/Day05/prime.py
@@ -10,9 +10,11 @@ import math
 
 for num in range(2, 100):
     is_prime = True
+    print("num=%d, int(math.sqrt(num))+1=%d" %(num, int(math.sqrt(num)) + 1))
     for factor in range(2, int(math.sqrt(num)) + 1):
+        print("factor=", factor)
         if num % factor == 0:
             is_prime = False
             break
     if is_prime:
-        print(num, end=' ')
+        print(num, " is prime\n")
